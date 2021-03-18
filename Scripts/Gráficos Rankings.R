ed3e7090b9b8e18f62db191edd38fe3b929107a1
Alberto Rodríguez R <albrodriguezr@unal.edu.co>
2021-03-17 19:06:51 -0500

    Carga rankings

--- Scripts/Gráficos Rankings.R
+++ Scripts/Gráficos Rankings.R
@@ -1,6 +1,11 @@
 # Librerías requeridas ----
+
 library(dplyr)
 library(readxl)
+library(tidyr)
+library(stringr)
+library(highcharter)
+library(htmlwidgets)
 
 # Función salvar widges ----
 
@@ -105,6 +110,106 @@ THEEmergentes <- Plot.SeriesRev(datos = THEEmergentesDF,
                    estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
                                 hc.Credits = "Periodo: 2018-2021"))
 
+# ARWUMundo ----
+
+
+ARWUMundoDF <- Rankings %>% filter(RANKING == 'ARWUMundo') %>% select(-c("RANKING"))
+
+ARWUMundo <- Plot.SeriesRev(datos = ARWUMundoDF, 
+                                categoria = "TOTAL", 
+                                col = c("#00BB2D"),
+                                libreria = "highcharter",
+                                titulo = "Evolución posiciones de la UNAL en Academic Ranking of World Universities
+                                          ARWU - Shanghai",
+                                labelY = "Puesto Mundo",
+                                estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                                 hc.Credits = "Periodo: 2014-2020"))
+
+
+# ARWULatino ----
+
+ARWULatinoDF <- Rankings %>% filter(RANKING == 'ARWULatino') %>% select(-c("RANKING"))
+
+ARWULatino <- Plot.SeriesRev(datos = ARWULatinoDF, 
+                            categoria = "TOTAL", 
+                            col = c("#00BB2D"),
+                            libreria = "highcharter",
+                            titulo = "Evolución posiciones de la UNAL en Latin America University Rankings
+                                      ARWU - Shanghai",
+                            labelY = "Puesto en Latinoamérica",
+                            estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                             hc.Credits = "Periodo: 2013-2020"))
+
+# MERCOEmpresas ----
+
+MERCOEmpresasDF <- Rankings %>% filter(RANKING == 'MERCOEmpresas') %>% select(-c("RANKING"))
+
+MERCOEmpresas <- Plot.SeriesRev(datos = MERCOEmpresasDF, 
+                             categoria = "TOTAL", 
+                             col = c("#00BB2D"),
+                             libreria = "highcharter",
+                             titulo = "Evolución posiciones de la UNAL en MERCO EMPRESAS",
+                             labelY = "Puesto en Colombia",
+                             estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                              hc.Credits = "Periodo: 2011-2020"))
+
+
+# MERCOTalento ----
+
+MERCOTalentoDF <- Rankings %>% filter(RANKING == 'MERCOTalento') %>% select(-c("RANKING"))
+
+MERCOTalento <- Plot.SeriesRev(datos = MERCOTalentoDF, 
+                                categoria = "TOTAL", 
+                                col = c("#00BB2D"),
+                                libreria = "highcharter",
+                                titulo = "Evolución posiciones de la UNAL en MERCO TALENTO",
+                                labelY = "Puesto en Colombia",
+                                estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                                 hc.Credits = "Periodo: 2011-2020"))
+
+# MERCORGC ----
+
+MERCORGCDF <- Rankings %>% filter(RANKING == 'MERCORGC') %>% select(-c("RANKING"))
+
+MERCORGC <- Plot.SeriesRev(datos = MERCORGCDF, 
+                               categoria = "TOTAL", 
+                               col = c("#00BB2D"),
+                               libreria = "highcharter",
+                               titulo = "Evolución posiciones de la UNAL en MERCO Responsabilidad y Gobierno Corporativo",
+                               labelY = "Puesto en Colombia",
+                               estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                                hc.Credits = "Periodo: 2013-2021"))
+
+
+# MERCOSector ----
+
+MERCOSectorDF <- Rankings %>% filter(RANKING == 'MERCOSector') %>% select(-c("RANKING"))
+
+MERCOSector <- Plot.SeriesRev(datos = MERCOSectorDF, 
+                           categoria = "TOTAL", 
+                           col = c("#00BB2D"),
+                           libreria = "highcharter",
+                           titulo = "Evolución posiciones de la UNAL en MERCO Sector Educación Superior",
+                           labelY = "Puesto en Colombia",
+                           estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                            hc.Credits = "Periodo: 2011-2020"))
+
+# USapiens ----
+
+USapiensDF <- Rankings %>% filter(RANKING == 'USapiens') %>% select(-c("RANKING"))
+
+USapiens <- Plot.SeriesRev(datos = USapiensDF, 
+                              categoria = "USAPIENS", 
+                              col = c("#8cc63f", "#f15a24", "#93278f"),
+                              libreria = "highcharter",
+                              titulo = "Evolución posiciones de las sedes de la UNAL en el Ranking U-Sapiens",
+                              labelY = "Puesto en Colombia",
+                              estilo    = list(hc.Tema = 4, hc.Slider = FALSE,
+                                               hc.Credits = "Periodo: 2011-2020"))
+
+
+
+
 # Crear HTML ----
 
 Salvar(QSMundo, "Rankings", "QSMundo.html")
@@ -113,3 +218,10 @@ Salvar(QSAreas, "Rankings", "QSAreas.html")
 Salvar(THEMundo, "Rankings", "THEMundo.html")
 Salvar(THELatino, "Rankings", "THELatino.html")
 Salvar(THEEmergentes, "Rankings", "THEEmergentes.html")
+Salvar(ARWUMundo, "Rankings", "ARWUMundo.html")
+Salvar(ARWULatino, "Rankings", "ARWULatino.html")
+Salvar(MERCOEmpresas, "Rankings", "MERCOEmpresas.html")
+Salvar(MERCOTalento, "Rankings", "MERCOTalento.html")
+Salvar(MERCORGC, "Rankings", "MERCORGC.html")
+Salvar(MERCOSector, "Rankings", "MERCOSector.html")
+Salvar(USapiens, "Rankings", "USapiens.html")
